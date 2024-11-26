7205fc3ab2df1c75ac2c22bbbf7553483b557cef
tsirina2 <tsirina2@comcast.net>
2024-11-26 15:15:46 -0500

    Add files via upload

---
+++ module2a.py
@@ -0,0 +1,11 @@
+x = int(input("Summa vklada v rublyax:"))
+p = int(input("Procent po vkladu:"))
+n = int(input("SUMMA K POLUCHENIYU:"))
+t = int(input("period vklada v godax:"))
+
+y = int(input("SUMMA VKLADA NUJNAYA:"))
+n = 0
+n = x + t * p
+while ( n < y ):
+    n =  n + 1
+    print (t)
