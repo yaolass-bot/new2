b8dc835f39cd0fcb7235589a62e4960cfd783c67
tsirina2 <tsirina2@comcast.net>
2024-11-29 10:27:51 -0500

    Add files via upload

---
+++ module321.py
@@ -0,0 +1,19 @@
+from tkinter import XView
+
+x = int(input("Povtorenie:"))
+n = int(input("CHISLO POVTORENIY:"))
+z = 0 #zadaem cykl while
+z = n * x # chto schitaem v cykle while
+while (z < n):
+    n = n + 1
+y = 0 # zadaem cykl for
+Y = 5 * x# ZADAEM CYKL FOR
+y = 5 * x
+print( n *'chastny sluchay cykla while')
+
+
+
+
+
+
+
