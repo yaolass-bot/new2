65503db39958df0e2f4c738a7663e630585599e9
tsirina2 <tsirina2@comcast.net>
2024-12-13 10:00:26 -0500

    Add files via upload

--- 622.py
+++ 622.py
@@ -1,8 +1,16 @@
-i = [1,4,1,6, "hello","a",5,"hello"]
-for i in range (0,len(i)):
-    i = sorted #
-    # [ i = i -1]
-    print (i)
+array = [1,4,1,6, "hello","a",5,"hello"]
+for i in range (0,len(array)):
+
+    for j in range (i + 1, len(array)):
+        if j == i + 1:
+            print (j)
+            print (i)
+
+
+
+
+
+
 
 
 
