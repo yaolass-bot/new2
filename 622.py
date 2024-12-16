42d9e5b051dfdead7bdd543712a710bf87741e79
tsirina2 <tsirina2@comcast.net>
2024-12-16 11:57:38 -0500

    Add files via upload

---
+++ 622.py
@@ -0,0 +1,35 @@
+array = [1,4,1,6, "hello","a",5,"hello"]
+for i in range (0,len(array)):
+    c = 0
+    for j in range (i + 1, len(array)):
+       if c == c + 1:
+           print (array[j])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
