8d78e3971cbc393f4c86d19ad02cffb30382805d
tsirina2 <tsirina2@comcast.net>
2024-12-23 16:00:10 -0500

    Add files via upload

--- 622.py
+++ 622.py
@@ -1,8 +1,10 @@
-array = [1,4,1,6, "hello","a",5,"hello"]
+array = [1,4,1,6,"hello","a",5,"hello"]
 for i in range (0,len(array)):
-    for j in range (i + 1, len(array)):
-       if array [j] == array [i == len(array)+1]:
+    for j in range (i + 1 , len(array)):
+        if array[j] == array[i]:
            print (array[j])
+
+
 
 
 
