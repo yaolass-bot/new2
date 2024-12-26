64980398372c79c3cac10beea73a1344da0b339b
tsirina2 <tsirina2@comcast.net>
2024-12-26 15:33:06 -0500

    Add files via upload

--- 642.PY.py
+++ 642.PY.py
@@ -1,5 +1,7 @@
-d = { 'name1': 'id1'}
-numbers = [1,2]
-    for i in range(0, len(numbers)):
-        if numbers[1] =numbers[len(numbers)]
-print d
+d = ["name1", "id1", "name2", "id2", "name3","id3"]
+new = [i for i in d if i % 2 == 0]
+new = [j for j in d if j % 2 != 0]
+print (new)
+
+
+
