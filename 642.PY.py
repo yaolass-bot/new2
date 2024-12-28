e336decd2eda5df37085b29b4f0c57286a159891
tsirina2 <tsirina2@comcast.net>
2024-12-28 17:03:49 -0500

    Add files via upload

--- 642.PY.py
+++ 642.PY.py
@@ -1,7 +1,6 @@
-d = ["name1", "id1", "name2", "id2", "name3","id3"]
-new = [i for i in d if i % 2 == 0]
-new = [j for j in d if j % 2 != 0]
-print (new) # [i,j]
+d = {"name1", "id1", "name2", "id2", "name3","id3"}
+for name  in d:
+    print ({"id","name"})
 
 
 
