319dbb5c8750602a301af2558588c62db3126fd4
tsirina2 <tsirina2@comcast.net>
2025-01-03 12:01:43 -0500

    Add files via upload

---
+++ 694.py
@@ -0,0 +1,54 @@
+users = {"alisa","bob", "charlie"}
+user_to_check = "bob"
+
+if user_to_check in users:
+    print(f"User {user_to_check} exists")
+else:
+    print(f"User {user_to_check} doesn't exist")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
