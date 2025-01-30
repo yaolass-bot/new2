352d638480d3c5d89a8bdcab5054fd8becc11d44
tsirina2 <tsirina2@comcast.net>
2025-01-30 15:42:10 -0500

    Update 702.py

--- 702.py
+++ 702.py
@@ -7,19 +7,19 @@ def merge_sort(array):
         merge_sort(right)
 
         i = j = k = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
+    while i < len(left) and j < len(right):
+     if left[i] < right[j]:
                 array[k] = left[i]
                 i += 1
-            else:
+     else:
                 array[k] = right[j]
                 j += 1
                 k += 1
-            while i < len(left):
+     while i < len(left):
                 array[k] = left[i]
                 i +=1
                 k +=1
-            while j < len(right):
+     while j < len(right):
                     array[k] = right[j]
                     j +=1
                     k +=1
