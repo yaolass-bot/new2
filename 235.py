f7dd346c55d64ff768b0f2d4b2ef27e6c19d95b8
tsirina2 <tsirina2@comcast.net>
2025-07-29 08:55:01 -0400

    Add files via upload

---
+++ 235.py
@@ -0,0 +1,10 @@
+array = [ 2, 6,7,8,9]
+array1 =[]
+max = array[0]
+for i in range(len(array)):
+    if array[i] > max:
+            maxi = array[i]
+            if maxi > array[0 + i]:
+                maxn = []
+array1.append(array[i])
+print(array1)
