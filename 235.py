365415dc2e3a110eb1a4dbb09ec56a77516b0b4e
tsirina2 <tsirina2@comcast.net>
2025-07-29 10:26:37 -0400

    Update 235.py

--- 235.py
+++ 235.py
@@ -1,10 +1,9 @@
 array = [ 2, 6,7,8,9]
 array1 =[]
 max = array[0]
-for i in range(len(array)):
-    if array[i] > max:
-            maxi = array[i]
-            if maxi > array[0 + i]:
-                maxi = []
+maxi = array[i]
+    for i in range(len(array)):
+            if array[i] > array [i + 1]:
+                     array[i] = array1[i]
 array1.append(array[i])
 print(array1)
